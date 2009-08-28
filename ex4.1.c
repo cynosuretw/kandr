2144cb12d150a20a7996a22e5252148ea0c094e2
cynosure <cynosure.chiou.gmail.com>
2009-08-28 15:25:09 +0800

    fix type error, remove so exe file.

--- ex4.1.c
+++ ex4.1.c
@@ -34,7 +34,7 @@ int getline(char s[], int lim)
 	int c, i;
 	i=0;
 
-	while (--lim > 0&& ( c = getchar() )!=EOF && c!='\n')
+	while (--lim > 0 && ( c = getchar() )!=EOF && c!='\n')
 		s[i++] = c;
 	if (c == '\n')
 		s[i++]= c;
--- makefile
+++ makefile
@@ -1,4 +1,5 @@
 all:
-	g++ ex1.9.c -o ex1.9
-	g++ ex1.10.c -o ex1.10
-	g++ ex2.1.c -o ex2.1
+	g++ ex1.9.c -o ex1.9.exe
+	g++ ex1.10.c -o ex1.10.exe
+	g++ ex2.1.c -o ex2.1.exe
+	g++ ex4.1.c -o ex4.1.exe
